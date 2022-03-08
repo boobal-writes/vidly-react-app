411a71f7448251fd565aefba76ac753b446e1b93
Boobalan Subramanian <boobal.sachin@gmail.com>
2022-03-09 00:46:10 +0530

    add movie form basic

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,20 +1,44 @@
 import React from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import Form from "./common/form";
+import Joi from "joi-browser";
 
-const MovieForm = () => {
-  const params = useParams();
-  const navigate = useNavigate();
-  return (
-    <React.Fragment>
-      <h1>Movie Form {params.id}</h1>
-      <button
-        className="btn btn-primary"
-        onClick={() => navigate("/movies", { replace: true })}
-      >
-        Save
-      </button>
-    </React.Fragment>
-  );
-};
+class MovieForm extends Form {
+  state = {
+    data: {
+      title: "",
+      numberInStock: "",
+      dailyRentalRate: "",
+    },
+    errors: {},
+  };
+
+  schema = {
+    title: Joi.string().required().label("Title"),
+    numberInStock: Joi.number()
+      .min(1)
+      .max(100)
+      .required()
+      .label("Number in Stock"),
+    dailyRentalRate: Joi.number().min(0.5).max(5).required().label("Rate"),
+  };
+
+  doSubmit = () => {
+    console.log("Submitted");
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Movie Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          {this.renderInput("numberInStock", "Number in Stock", "number")}
+          {this.renderInput("dailyRentalRate", "Rate", "number")}
+          {this.renderButton("Save")}
+        </form>
+      </React.Fragment>
+    );
+  }
+}
 
 export default MovieForm;
